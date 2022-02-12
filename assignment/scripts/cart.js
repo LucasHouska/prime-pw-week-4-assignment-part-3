7147a1f8ee266d1ac117c099dab772e692e662bc
Lucas Houska <ljhouska75@gmail.com>
2022-02-12 13:36:54 -0600

    Initial commit Part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,31 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+console.log(basket)
+
+function addItem( item ) {
+  basket.push(item);
+  return true;
+}
+
+console.log(addItem('apples'))
+console.log(addItem('bananas'));
+
+function listItems( array ) {
+  for (var i = 0; i < array.length; i++) {
+    console.log(array[i])
+  }
+}
+
+listItems(basket)
+
+
+function empty() {
+  basket = []
+}
+
+empty()
+console.log(basket)
